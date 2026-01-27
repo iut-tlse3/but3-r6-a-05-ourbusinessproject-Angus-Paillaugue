553e0be78eb6eb2246a73be24a65f9fe88660529
Angus-Paillaugue <angus@paillaugue.fr>
2026-01-27 11:33:51 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,11 +1,19 @@
 package ourbusinessproject;
 
-import jakarta.validation.Valid;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.springframework.validation.annotation.Validated;
 
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     @NotNull
     @Size(min=1)
     private String title;
